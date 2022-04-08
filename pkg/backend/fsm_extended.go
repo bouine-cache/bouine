109fe2e1c3b0c0ce4b4970e1c147fe559e34b67c
thylong <theotime@protonmail.com>
2022-04-08 18:50:30 +0200

    FSM: Add a TODO comment on current Purge logic.

--- pkg/backend/fsm_extended.go
+++ pkg/backend/fsm_extended.go
@@ -49,6 +49,7 @@ func (rr *RaftedBadger) applyCacheEntry(req *pb.AddCacheEntryRequest) error {
 
 // applyCacheEntryRequest applies PurgeCacheRequest on FSM.
 func (rr *RaftedBadger) applyPurgeCache() error {
+	// TODO: Prevent Purge to threaten quorum stability
 	err := rr.BadgerKV.DropAll()
 	if err != nil {
 		rr.Logger.Error("applyPurgeCache err", zap.String("component", "raft"), zap.Error(fmt.Errorf("fail to apply on FSM: %s", err)))

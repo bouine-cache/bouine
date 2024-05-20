3c55e793cf64dee81f36ce7baf42484a0d3655b2
thylong <theotime@protonmail.com>
2024-05-20 18:21:28 +0200

    feat(stale): basic stale state testing

---
+++ pkg/middlewares/stale_test.go
@@ -0,0 +1,45 @@
+package middlewares
+
+import (
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	fiber "github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/proxy"
+	"github.com/gofiber/fiber/v2/utils"
+	"github.com/valyala/fasthttp"
+	"go.uber.org/zap"
+)
+
+func Test_Stale_Healthy_Flow(t *testing.T) {
+	t.Parallel()
+
+	target, addr := createUpstreamBackendServer(
+		func(c *fiber.Ctx) error { return c.SendStatus(fiber.StatusTeapot) }, t,
+	)
+
+	resp, err := target.Test(httptest.NewRequest("GET", "/", nil), -1)
+	utils.AssertEqual(t, nil, err)
+	utils.AssertEqual(t, fiber.StatusTeapot, resp.StatusCode)
+
+	app := fiber.New()
+	app.Use(ClassicHealthcheckMiddleware(Config{
+		Period:          70 * time.Millisecond,
+		Logger:          zap.NewExample(),
+		Upstreams:       []string{addr},
+		HealthcheckKind: smartHealthcheckKind,
+		Client:          &fasthttp.Client{},
+	}))
+	app.Use(ProxyMiddleware(proxy.Config{Servers: []string{addr}}))
+
+	// First request creates the upstream entry
+	resp, err = app.Test(httptest.NewRequest("GET", "/", nil), -1)
+	utils.AssertEqual(t, nil, err)
+	utils.AssertEqual(t, fiber.StatusTeapot, resp.StatusCode)
+
+	// Fire another request with upstream tagged as unhealthy (unreachable)
+	resp, err = app.Test(httptest.NewRequest("GET", "/", nil), -1)
+	utils.AssertEqual(t, nil, err)
+	utils.AssertEqual(t, fiber.StatusTeapot, resp.StatusCode)
+}

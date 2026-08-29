34969e662ae6713937560d61a687904879333fba
thylong <theotime@protonmail.com>
2026-08-29 23:04:01 +0200

    chore(bench): pin cacheable-miss budget at 13 allocs/op
    The batch2 MISS-path work brought BenchmarkGate_Handler_CacheMiss_
    Cacheable from 21 to 13 allocs/op (budget was 23). Pin the new
    floor so regressions are caught.
    
    Signed-off-by: thylong <thylong@users.noreply.github.com>
    Signed-off-by: thylong <theotime@protonmail.com>

--- bench/run.sh
+++ bench/run.sh
@@ -28,7 +28,7 @@
 #   Evaluate_Hit:                     0
 #   HotStore_Get_Hit:                 0
 #   Handler_CacheHit_ReusableWriter:  0  (zero-alloc hit path achieved)
-#   Handler_CacheMiss_Cacheable:      23
+#   Handler_CacheMiss_Cacheable:      13  (batch2: raw-header precheck, no unique-key interning, sharded singleflight)
 #   SIEVE_Access:                     0
 #   Cachaner_Access:                   0
 #   Cachaner_AccessSlowPath:           0
@@ -60,7 +60,7 @@ declare -A BUDGETS=(
     [Evaluate_Hit]=0
     [HotStore_Get_Hit]=0
     [Handler_CacheHit_ReusableWriter]=0
-    [Handler_CacheMiss_Cacheable]=23
+    [Handler_CacheMiss_Cacheable]=13
     [SIEVE_Access]=0
     [Cachaner_Access]=0
     [Cachaner_AccessSlowPath]=0
